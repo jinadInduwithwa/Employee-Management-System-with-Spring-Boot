73d1330295c59cebe18afb8b749a6b1a1a3d0c4d
JINAD INDUWITHWA A.G <140488745+jinadInduwithwa@users.noreply.github.com>
2024-11-28 09:44:00 +0530

    update application.properties file
    eshtablish database connection

---
+++ src/main/java/com/example/Employee_Management_System_with_Spring_Boot/EmployeeManagementSystemWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.Employee_Management_System_with_Spring_Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystemWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Employee_Management_System_with_Spring_Boot/EmployeeManagementSystemWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Employee_Management_System_with_Spring_Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
